5834e342d750f0928ad4ea1dd164cf131aa48a5f
Pouria Almassi <pouriaalmassi@gmail.com>
2018-02-25 22:29:21 +1100

    Initial Commit

---
+++ SwiftChain/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftChain
+//
+//  Created by Pouria Almassi on 25/2/18.
+//  Copyright © 2018 Pouria Almassi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
