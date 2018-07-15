c2cb446cf8b27618117618e0c5b4c75bd7b76b7c
Pouria Almassi <pouriaalmassi@gmail.com>
2018-07-15 17:16:52 +1000

    Project creation and adding CryptoSwift dependency.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftChain",
     dependencies: [
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", versions: Version(0,8,0)..<Version(0,8,3))
+        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", Version("0.9.0"))
     ]
 )
