aacc17e7e0a745143705690e6ac3e643340a0d63
Pouria B. Almassi <pouriaba@gmail.com>
2018-07-19 09:13:23 +1000

    Add the ability to add a transaction with metadata.

--- SwiftChain/Controllers/ViewController.swift
+++ SwiftChain/Controllers/ViewController.swift
@@ -11,9 +11,6 @@ import UIKit
 final class ViewController: UIViewController {
     private let server = Server(blockchain: Blockchain())
     @IBOutlet var textView: UITextView!
-    @IBOutlet var amountLabel: UILabel!
-    @IBOutlet var senderLabel: UILabel!
-    @IBOutlet var recipientLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +18,35 @@ final class ViewController: UIViewController {
     }
 
     @IBAction func addTransaction(_ sender: UIButton) {
-        // guard
-        //     let amountString = amountLabel.text,
-        //     let amountValue = Double(amountString),
-        //     let senderString = senderLabel.text,
-        //     let recipientString = recipientLabel.text else { return }
-        // server.addTransaction(with: Transaction(sender: senderString, recipient: recipientString, amount: amountValue))
-        server.addTransaction(with: Transaction(sender: "me", recipient: "you", amount: 500.0))
+        let alert = UIAlertController(title: "",
+                                      message: nil,
+                                      preferredStyle: .alert)
+
+        alert.addTextField { tf in tf.placeholder = "Sender" }
+        alert.addTextField { tf in tf.placeholder = "Recipient" }
+        alert.addTextField { tf in tf.placeholder = "Amount"; tf.keyboardType = .decimalPad }
+
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+            guard
+                let `self` = self,
+                let textFields = alert.textFields,
+                let senderString = textFields[0].text,
+                let recipientString = textFields[1].text,
+                !senderString.isEmpty,
+                !recipientString.isEmpty,
+                let amountString = textFields[2].text,
+                let amountValue = Double(amountString)
+                else { return }
+
+            self.server.addTransaction(with: Transaction(sender: senderString,
+                                                         recipient: recipientString,
+                                                         amount: amountValue))
+        }
+
+        alert.addAction(addAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(alert, animated: true, completion: nil)
     }
 
     @IBAction func mine(_ sender: UIButton) {
